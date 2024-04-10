3c478fb9406bf2c325c85022294ff6dc123186d3
OjedaErwin123 <ojedaerwinalejandro@gmail.com>
2024-04-09 23:30:22 -0400

    codigo

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+
+using namespace std;
+
+int main() {
+  float alto, largo, precioM2, area, precioTotal;
+
+  cout<<"Ingrese el alto del lote: ";
+  cin>>alto;
+  cout<<"Ingrese el largo del lote: ";
+  cin>>largo;
+  cout<<"Ingrese el precio por metro cuadrado: ";
+  cin>>precioM2;
+
+  area = alto * largo;
+  precioTotal = area * precioM2;
+
+  cout<<"El área del lote es: " << area << " metros cuadrados." << endl;
+  cout<<"El precio total del lote es: $" << precioTotal << endl;
+
+  return 0;
+}
